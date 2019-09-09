0dd2427e1b1596df51a6dadb0c04f18f65e8137c
Chirag Pandit <chirag.pandit@here.com>
2019-09-09 23:35:22 +0530

    WCopy1

--- test_local.js
+++ test_local.js
@@ -50,101 +50,172 @@ let styleGroupsConfig = {
     }
 }
 
-define(function (require) {
-
-    var basePath = 'http://127.0.0.1:7000/test_local.js'//
-    var UNDEF;
-    var drawingPlugin = {
-        init: function (pI, config) {
-            console.log(pI.getLanguage());
-
-            
-            // //Get Map Position on clicking that map
-
-            // pI.sandwichMap().addEventListener('pointerup', function (e) {
-            //     var obj = e.target;
-            //     console.log(obj)
-            //     let latitude = obj.geometry.coordinates[1];
-            //     let longitude = obj.geometry.coordinates[0];
-            //     let coords = {
-            //         latitude, longitude
-            //     }
-            //     console.log("Test : "+ coords)
-            // });
-            // pI.sandwichMap().addEventListener('dblclick', function (e) {
-            //     var obj = e.target;
-            //     console.log(obj)
-            //     let latitude = obj.geometry.coordinates[1];
-            //     let longitude = obj.geometry.coordinates[0];
-            //     let coords = {
-            //         latitude, longitude
-            //     }
-            //     console.log("Drawing Completed : "+coords)
-            //     console.log("Finished")
-            // });
-
-            // var YOUR_ACCESS_TOKEN = "IzOHLHLyynLPwG28V26Hcw";//window.parent.YOUR_ACCESS_TOKEN;
-
-            // window.display = HERE.xyz.maps.Map
-            
-            // new here.xyz.maps.Map(
-            //     document.getElementById("map"),
-            //     {
-            //         zoomLevel: 17,
-            //         center: {
-            //             longitude: -122.48024, latitude: 37.77326
-            //         },
-            //         // add layers to display
-            //         layers: [
-            //             new here.xyz.maps.layers.TileLayer({
-            //                 name: 'Image Layer',
-            //                 min: 1,
-            //                 max: 20,
-            //                 // image layer for displaying satellite images
-            //                 provider: new here.xyz.maps.providers.ImageProvider({
-            //                     name: 'Live Map',
-            //                     url: 'https://{SUBDOMAIN_INT_1_4}.mapcreator.tilehub.api.here.com/tilehub/wv_livemap_bc/png/sat/256/{QUADKEY}?access_token=' + YOUR_ACCESS_TOKEN
-            //                 })
-            //             })
-            //         ]
-            //     }
-            // );
-
-            const editorConfig = {
-                featureSelectionByDefault: false,
-                keepFeatureSelection: true
-            };
-
-            var myEditor = new HERE.xyz.maps.editor.Editor(window.display, editorConfig);
-            window.editor = myEditor
-            myEditor.clearObjectSelection();
-
-            //Enable Drawing Board in Polygon mode
-
-            window.drawingBoard = myEditor.getDrawingBoard();
-            window.drawingBoard.start({
-                mode: HERE.xyz.maps.editor.features.Area,
-                layer: HERE.xyz.maps.layers.TileLayer,//HERE.xyz.maps.layers.TileLayer,//this.state.currentLayer,
-                styleGroup: styleGroupsConfig.drawingBoard.polygons
-            })
+define(
 
 
-            // pI.sandwichMap().addEventListener('tap', function (e) {
-            //     console.log(e)
+    function (require) {
 
-            // });
+        // onMapDoubleClick = e => {
+        //     // this.mapObject.setZoomlevel(
+        //     //     this.mapObject.getZoomlevel() + 1,
+        //     //     e.mapX,
+        //     //     e.mapY
+        //     // );
+        //     myEditor.cancel()
+        //     myEditor.clearObjectSelection()
+        // };
 
-            // pI.sandwichMap().addEventListener('dbltap', function (e) {
-            //     console.log(e)
 
-            // });
+        // onEditorHistoryChange = () => {
 
-            // pI.sandwichMap().addEventListener('pointerup', function (e) {
-            //     //e.getMapPosition()
+        //     history = {
+        //         current_step: this.editor.get('history.current'),
+        //         total_steps: this.editor.get('history.length'),
+        //         modified_features: this.editor.get('changes.length'),
+        //     }
+        //     console.log(history);
 
-            // });
+        //     // window.drawingBoard.cancel();
+        // };
 
+
+        onKeyPress = e => {
+            console.log("Key Pressed onKeyPress..")
+            //if (this.editor.active())
+            //if (myEditor.isEditMode()) {
+            e.preventDefault();
+
+            switch (e.keyCode) {
+                case 27: // ESCAPE
+                // this.clearDrawingBoard();
+                window.drawingBoard.cancel();
+                break;
+                case 8://Backspace
+                this.deleteDrawing();
+                break;
+                case 13://ENTER
+                this.deleteDrawing();
+                break;
+                default:
+            }
+        
+        };
+
+        var basePath = 'http://127.0.0.1:7000/test_local.js'//
+        var UNDEF;
+        var drawingPlugin = {
+            init: function (pI, config) {
+                console.log(pI.getLanguage());
+
+
+                // //Get Map Position on clicking that map
+
+                // pI.sandwichMap().addEventListener('pointerup', function (e) {
+                //     var obj = e.target;
+                //     console.log(obj)
+                //     let latitude = obj.geometry.coordinates[1];
+                //     let longitude = obj.geometry.coordinates[0];
+                //     let coords = {
+                //         latitude, longitude
+                //     }
+                //     console.log("Test : "+ coords)
+                // });
+                // pI.sandwichMap().addEventListener('dblclick', function (e) {
+                //     var obj = e.target;
+                //     console.log(obj)
+                //     let latitude = obj.geometry.coordinates[1];
+                //     let longitude = obj.geometry.coordinates[0];
+                //     let coords = {
+                //         latitude, longitude
+                //     }
+                //     console.log("Drawing Completed : "+coords)
+                //     console.log("Finished")
+                // });
+
+                // var YOUR_ACCESS_TOKEN = "IzOHLHLyynLPwG28V26Hcw";//window.parent.YOUR_ACCESS_TOKEN;
+
+                // window.display = HERE.xyz.maps.Map
+
+                // new here.xyz.maps.Map(
+                //     document.getElementById("map"),
+                //     {
+                //         zoomLevel: 17,
+                //         center: {
+                //             longitude: -122.48024, latitude: 37.77326
+                //         },
+                //         // add layers to display
+                //         layers: [
+                //             new here.xyz.maps.layers.TileLayer({
+                //                 name: 'Image Layer',
+                //                 min: 1,
+                //                 max: 20,
+                //                 // image layer for displaying satellite images
+                //                 provider: new here.xyz.maps.providers.ImageProvider({
+                //                     name: 'Live Map',
+                //                     url: 'https://{SUBDOMAIN_INT_1_4}.mapcreator.tilehub.api.here.com/tilehub/wv_livemap_bc/png/sat/256/{QUADKEY}?access_token=' + YOUR_ACCESS_TOKEN
+                //                 })
+                //             })
+                //         ]
+                //     }
+                // );
+
+                //Set up Basemap
+                this.mapObject = window.display;//Using MapCreator's Initialized Map
+                //  new HERE.xyz.maps.Map(th is.refs.container, {
+                //     credentials: { access_token: getToken() },
+                //     zoomLevel: settings.zoom,
+                //     center: { latitude: settings.center[0], longitude: settings.center[1] },
+                //     layers: [this.baseLayer],
+                //   });
+
+                window.mapObject = this.mapObject;
+
+                //Setup Editor
+                const editorConfig = {
+                    featureSelectionByDefault: false,
+                    keepFeatureSelection: true
+                };
+
+                var myEditor = new HERE.xyz.maps.editor.Editor(window.display, editorConfig);
+                window.editor = myEditor
+                myEditor.clearObjectSelection();
+
+                //Enable Drawing Board in Polygon mode
+
+                window.drawingBoard = myEditor.getDrawingBoard();
+
+                window.drawingBoard.create();
+
+                window.drawingBoard.start({
+                    mode: HERE.xyz.maps.editor.features.Area,
+                    layer: HERE.xyz.maps.layers.TileLayer,//HERE.xyz.maps.layers.TileLayer,//this.state.currentLayer,
+                    styleGroup: styleGroupsConfig.drawingBoard.polygons
+                })
+
+                // this.mapObject.addEventListener('dbltap', this.onMapDoubleClick);
+                // myEditor.addEventListener('pointerup', this.onEditorPointerUp);
+                // myEditor.addObserver('history.current', this.onEditorHistoryChange);
+
+                document.addEventListener('keydown', this.onKeyPress);
+
+                // myEditor.setZoomLevel(19)
+
+                // pI.sandwichMap().addEventListener('tap', function (e) {
+                //     console.log(e)
+
+                // });
+
+                // pI.sandwichMap().addEventListener('dbltap', function (e) {
+                //     console.log(e)
+
+                // });
+
+                // pI.sandwichMap().addEventListener('pointerup', function (e) {
+                //     //e.getMapPosition()
+
+                // });
+
+            }
         }
-    }
-    return drawingPlugin;
-});
+        return drawingPlugin;
+    });
